3c824642f84783b68f110bd679c57dfede10d2b9
RoyAWilson <royawilson@hotmail.co.uk>
2025-08-07 18:33:17 +0100

    Up to login form showing correctly -- next to get it working

--- website/forms.py
+++ website/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    email = forms.forms.EmailField(Label="", widget=forms.textinput(attrs={'class':'form-control', 'placeholder': 'Email Address'}), text="", required=False)
-    first_name = forms.CharField(Label="", max_length=100, widget=forms.textinput(attrs={'class':'form-control', 'placeholder': 'First Name'}), required=False)
-    last_name = forms.CharField(Label="", max_length=100, widget=forms.textinput(attrs={'class':'form-control', 'placeholder': 'Last Name'}), required=False)
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Email Address'}), required=False)
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'First Name'}), required=False)
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Last Name'}), required=False)
 
     class Meta:
         model = User
