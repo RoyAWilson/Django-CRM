4bd34d648c197c1fee965a185bf4a799d0d4b471
RoyAWilson <royawilson@hotmail.co.uk>
2025-08-08 20:36:25 +0100

    Sign up Form finally working problem 1 tiny spelling error!  Couldn't spot it till I had been a way for a while.

--- website/views.py
+++ website/views.py
@@ -28,16 +28,16 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        form=SighUpForm(request.POST)
+        form=SignUpForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             # Authenticate and login:
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password = password)
             login(request, user)
-            messages.success('You have successfully registered.  Welcome')
-            return redirect(request, 'home')
+            messages.success(request, message='You have successfully registered.  Welcome')
+            return redirect('home')
     else:
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
